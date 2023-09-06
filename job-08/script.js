58da0dfe6d9fdc5cb68282255a6e5f29054e0f62
JB <jean-bernard.laguerre@laplateforme.io>
2023-09-07 00:10:20 +0200

    job 08

--- job-08/script.js
+++ job-08/script.js
@@ -1,21 +1,48 @@
 function myArraySort(array, sorting) {
 
     let change = 0
-    for (let i = 0; i < long(array); i++) {
 
-        switch (sorting) {
-            case "":
-                break
-            case "":
-                break
+    while (true) {
+        for (let i = 0; i < long(array); i++) {
+
+            let condition
+            switch (sorting) {
+                case "ASC":
+                    condition = (array[i] > array[i+1])
+                    break
+                case "DESC":
+                    condition = (array[i] < array[i+1])
+                    break
+                default:
+                    condition = false
+            }
+
+            if (condition) {
+                let tmp = array[i]
+                array[i] = array[i+1]
+                array[i+1] = tmp
+
+                change++
+            }
+        }
+
+        if(change == 0){
+            break
+        } else {
+            change = 0
         }
     }
+    
+    return array
 }
 
 function long(array) {
     count = 0
-    for(let item of arrray) {
+    for(let item of array) {
         count++
     }
     return count
 }
+
+console.log(myArraySort([1,5,67,12,3], "DESC"))
+console.log(myArraySort([1,5,67,12,3], "ASC"))
